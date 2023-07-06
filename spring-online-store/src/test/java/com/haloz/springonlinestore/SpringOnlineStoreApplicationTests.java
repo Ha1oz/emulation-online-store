470113dd105d3a2b0bbe9b92bc6a6867442df6f5
Ha1oz <oleg.barkalov@gmail.com>
2023-07-06 13:33:53 +0300

    feat: first steps

---
+++ spring-online-store/src/test/java/com/haloz/springonlinestore/SpringOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haloz.springonlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
